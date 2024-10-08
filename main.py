4456724ea5b3a77dafb688e6a343ed603cbcff82
tevoshw <estettv155@gmail.com>
2024-10-08 19:09:59 -0300

    Update main.py
    add a search_aurora function

--- main.py
+++ main.py
@@ -54,3 +54,22 @@ if st_au:
     und_user_au = understanding_user_aurora()
     if und_user_au:
         print(f"User input: {und_user_au}")
+
+
+
+
+def search_aurora(searchi):
+    print("Lets search this for you my brother!")
+    time.sleep(1.5)
+    url = f"https://www.google.com/search?q={searchi}"
+    webbrowser.open(url)
+
+
+search_term = ''
+if und_user_au:
+    for sinonimo in pesquisar_e_sinonimos:
+        if sinonimo in und_user_au:
+            search_term = und_user_au.split(sinonimo, 1)[-1].strip()  # Limita a divisão a 1
+            if search_term:  # Verifica se o termo não está vazio
+                search_aurora(search_term)
+            break 
