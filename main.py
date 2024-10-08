79f14e5670d5e39c67728b19b2ba8bd09b4a35bc
tevoshw <estettv155@gmail.com>
2024-10-08 18:31:53 -0300

    aurora fix bugs
    add a new function understandind_user

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
 import speech_recognition as sr
+import os
 import time
+import webbrowser
+
+
+
 
 
 def start_aurora():
     rec = sr.Recognizer()
     with sr.Microphone() as mic:
         rec.adjust_for_ambient_noise(mic)
-        print("Fale em 3...")
-        time.sleep(1)
-        print("Fale em 2...")
-        time.sleep(1)
-        print("Fale em 1...")
-        time.sleep(1)
         audio = rec.listen(mic)
 
     try:
-        texto = rec.recognize_google(audio, language='pt-BR')
-        texto = texto.lower()
-        print(f"Você disse: {texto}")
-        return texto  # Retorna o texto para uso posterior
+        texto = rec.recognize_google(audio, language='pt-BR').lower()
+        if 'aurora' in texto:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Aurora, is ONLINE!")
+            time.sleep(1)
+            print("Let's go!")
+            return texto.upper 
     except sr.UnknownValueError:
-        print("Não entendi.")
-        return ""
-    except sr.RequestError:
-        print("Erro ao conectar ao serviço de reconhecimento.")
+        print("I don't understand sorry.")
         return ""
 
 
-start_aurora()
 
 
 
 
 
+
+def understanding_user_aurora():
+    print("How can i help boss?")
+    rec = sr.Recognizer()
+    with sr.Microphone() as mic:
+        rec.adjust_for_ambient_noise(mic)
+        audio = rec.listen(mic)
+
+
+    try:
+        texto = rec.recognize_google(audio, language='pt-BR').lower()
+        time.sleep(1)
+        print(f"You said  {texto.upper()}")
+    except sr.UnknownValueError:
+        print("I don't understand sorry.")
+        return ""
