5f41f28f4b28c77f6069ae6899f3c0b15cf329fc
tevoshw <estettv155@gmail.com>
2024-10-09 18:26:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,31 @@
 import speech_recognition as sr
 import os
 import webbrowser
+import requests
+from bs4 import BeautifulSoup
 import pyttsx3
+from googlesearch import search
 
 ########################################################
 pesquisar_e_sinonimos = {
     'sobre': 1.8,
     'me ajude a encontrar': 1.9,
-
+    'para mim': 1.9,
+    'para': 1.9,
     'pesquisar': 1.0,
     'pesquise': 1.0,
     'pesquisa': 1.0,
-
     'buscar': 1.1,
     'busca': 1.1,
     'busque': 1.1,
-
     'procurar': 1.2,
     'procura': 1.2,
     'procure': 1.2,
-
     'encontrar': 1.3,
     'encontre': 1.3,
-    'encontra': 1.3,
-
     'investigar': 1.4,
-    'investigue': 1.4,
-    'investiga': 1.4,
-
     'analisar': 1.5,
-    'analise': 1.5,
-    'analisa': 1.5,
-
     'consultar': 1.6,
-    'consulte': 1.6,
-    'consulta': 1.6,
 }
 
 ########################################################
@@ -44,7 +35,7 @@ dic_iniciar_seg = {
     'sexta ': 0,
     'sexta-feira': 0,
     'sexta': 0,
-    }
+}
 
 ########################################################
 
@@ -55,12 +46,7 @@ sociais = {
     'obrigada': 1.7,
     'por gentileza': 1.6,
     'por obséquio': 1.6,
-    'por obsequio': 1.6,
 }
-
-
-
-
 
 ########################################################
 def inicar_sexta():
@@ -75,7 +61,7 @@ def inicar_sexta():
     try:
         if any(palavra in texto for palavra in dic_iniciar_seg):
             seg_iniciar_resposta = "Sexta-feira está ativa agora, vamos prosseguir!"
-            print(f'{seg_iniciar_resposta.upper()} \nAGUARDE ATE A MENSAGEM DE VOZ TERMINAR!')
+            print(f'{seg_iniciar_resposta.upper()} \nAGUARDE ATÉ A MENSAGEM DE VOZ TERMINAR!')
             print('----------------------------------------------')
             engine = pyttsx3.init()
             engine.setProperty('voice', engine.getProperty('voices')[0].id)
@@ -90,74 +76,96 @@ def inicar_sexta():
         engine.say(seg_iniciar_erro)
         engine.runAndWait()
 
-st_au = inicar_sexta()
+inicar_sexta_variavel = inicar_sexta()
+
 ########################################################
 
-def understanding_user_iris():
-    au_start_answer = "Como posso te ajudar chefe?!"
-
+def entender_user_seg():
+    seg_iniciada_resposta = "Como posso te ajudar chefe?!"
     engine = pyttsx3.init()
     engine.setProperty('voice', engine.getProperty('voices')[0].id)
-    engine.say(au_start_answer)
+    engine.say(seg_iniciada_resposta)
     engine.runAndWait()
 
-    rec = sr.Recognizer()
+    reconhecer = sr.Recognizer()
     with sr.Microphone() as mic:
-        rec.adjust_for_ambient_noise(mic)
+        reconhecer.adjust_for_ambient_noise(mic)
         print("SEXTA-FEIRA: Pode falar")
-        audio = rec.listen(mic)
+        audio = reconhecer.listen(mic)
     try:
-        texto = rec.recognize_google(audio, language='pt-BR').lower()
+        texto = reconhecer.recognize_google(audio, language='pt-BR').lower()
         print(f"VOCÊ DISSE: {texto}")
         print('----------------------------------------------')
         return texto
     except sr.UnknownValueError:
-        iris_start_error = "Não entendi, tente novamente!"
+        seg_iniciada_erro = "Não entendi, tente novamente!"
         engine = pyttsx3.init()
         engine.setProperty('voice', engine.getProperty('voices')[0].id)
-        engine.say(iris_start_error)
+        engine.say(seg_iniciada_erro)
         engine.runAndWait()
         return ''
 
-und_user_au = ''
-if st_au:
-    und_user_au = understanding_user_iris()
+def pesquisar_segunda(pesquisarsite):
+    segunda_pesquisar_voz = "Você deseja que eu leia, ou te redirecione para a aba? (Responde com leia ou redirecione)"
+    engine = pyttsx3.init()
+    engine.setProperty('voice', engine.getProperty('voices')[0].id)
+    engine.say(segunda_pesquisar_voz)
+    engine.runAndWait()
 
+    receconhecer = sr.Recognizer()
+    with sr.Microphone() as mic:
+        receconhecer.adjust_for_ambient_noise(mic)
+        print("SEXTA FEIRA: Pode Falar")
+        audio = receconhecer.listen(mic)
+        texto = receconhecer.recognize_google(audio, language='pt-BR').lower()
+    
+    if 'leia' in texto:
+        print(f"VOCÊ DISSE: {texto}")
+        # Realiza a pesquisa e lê o conteúdo do primeiro site
+        for url in search(pesquisarsite, num_results=1):
+            print(f"Acessando: {url}")
+            ler_site(url)
+    elif 'redirecione' in texto:
+        print(f"VOCÊ DISSE: {texto}")
+        webbrowser.open(f"https://www.google.com/search?q={pesquisarsite}")
+
+def ler_site(url):
+    try:
+        resposta = requests.get(url)
+        resposta.raise_for_status() 
+        sopa = BeautifulSoup(resposta.text, 'html.parser')
+        
+
+        paragrafos = sopa.find_all('p')
+        
+        texto = ' '.join(paragrafo.get_text(strip=True) for paragrafo in paragrafos)
+
+        if texto:
+            engine = pyttsx3.init()
+            engine.say(texto)
+            engine.runAndWait()
+        else:
+            print("Nenhum parágrafo encontrado.")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao acessar o site: {e}")
 
 ########################################################
 
-def search_aurora(searchi):
+entender_user_seg_variavel = ''
+if inicar_sexta_variavel:
+    entender_user_seg_variavel = entender_user_seg()
 
-    iris_search_voice = "Você deseja que eu leia, ou te redirecione para a aba? (Responde com leia ou  redirecione)"
-    engine = pyttsx3.init()
-    engine.setProperty('voice', engine.getProperty('voices')[0].id)
-    engine.say(iris_search_voice)
-    engine.runAndWait()
-
-    rec = sr.Recognizer()
-    with sr.Microphone() as mic:
-        rec.adjust_for_ambient_noise(mic)
-        print("SEXTA FEIRA: Pode Falar")
-        audio = rec.listen(mic)
-        texto = rec.recognize_google(audio, language='pt-BR').lower()
-    if 'leia' in texto:
-        print(f"VOCÊ DISSE: {texto}")
-    elif 'redirecione' in texto:
-        print(f"VOCÊ DISSE: {texto}")
-        webbrowser.open(f"https://www.google.com/search?q={searchi}")
-
-search_term = ""
-
-if und_user_au:
+if entender_user_seg_variavel:
     for sinonimo in pesquisar_e_sinonimos:
-        if sinonimo in und_user_au:
-            search_term = und_user_au.split(sinonimo, 1)[-1].strip()
-            if search_term:
+        if sinonimo in entender_user_seg_variavel:
+            procurar_termo = entender_user_seg_variavel.split(sinonimo, 1)[-1].strip()
+            if procurar_termo:
                 for social in sociais:
-                    if search_term.endswith(social):
-                        search_term = search_term[:-len(social)].strip()  
+                    if procurar_termo.endswith(social):
+                        procurar_termo = procurar_termo[:-len(social)].strip()  
                         break  
 
-                search_term = search_term.lstrip(',').strip() 
-                search_aurora(search_term)
+                procurar_termo = procurar_termo.lstrip(',').strip() 
+                pesquisar_segunda(procurar_termo)
             break
