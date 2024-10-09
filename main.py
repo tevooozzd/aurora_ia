19ac825abf23899a92a52133bab2318e08c94320
tevoshw <estettv155@gmail.com>
2024-10-09 15:48:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,75 +2,105 @@ import speech_recognition as sr
 import os
 import time
 import webbrowser
+from gtts import gTTS
+import pyttsx3
 
 
 
+########################################################
+pesquisar_e_sinonimos = [
+    "pesquisar", "pesquisa", "pesquise",
+    "buscar", "busque", "busca",
+    "procurar", "procure", "procura",
+    "encontrar", "encontre", "encontra",
+    "investigar", "investigue", "investiga",
+    "analisar", "analise", "analisa",
+    "examinar", "examine", "examina",
+    "estudar", "estude", "estuda",
+    "consultar", "consulte", "consulta",
+    "explorar", "explore", "explora",
+    "averiguar", "averigüe", "averigua"
+]
+
+########################################################
 
 
-def start_aurora():
+########################################################
+def start_iris():
     rec = sr.Recognizer()
     with sr.Microphone() as mic:
         rec.adjust_for_ambient_noise(mic)
+        print("Pode falar")
         audio = rec.listen(mic)
 
     try:
         texto = rec.recognize_google(audio, language='pt-BR').lower()
-        if 'aurora' in texto:
+        if 'segunda' in texto:
             os.system('cls' if os.name == 'nt' else 'clear')
-            au_start_answer = "Aurora está ativa agora, como posso ajudar?!"
+            iris_start_answer = "Segunda-feira está ativa agora, vamos prosseguir!"
             engine = pyttsx3.init()
             engine.setProperty('voice', engine.getProperty('voices')[0].id)
-            engine.say(au_start_answer)
+            engine.say(iris_start_answer)
             engine.runAndWait()
+            return True
     except sr.UnknownValueError:
-        print("I don't understand sorry.")
-        return ""
 
+        iris_start_error = "Não entendi, tente novamente!"
+        engine = pyttsx3.init()
+        engine.setProperty('voice', engine.getProperty('voices')[0].id)
+        engine.say(iris_start_error)
+        engine.runAndWait()
+        return False
 
+st_au = start_iris()
 
+########################################################
 
+def understanding_user_iris():
 
+    au_start_answer = "Como posso te ajudar chefe?!"
 
+    engine = pyttsx3.init()
+    engine.setProperty('voice', engine.getProperty('voices')[0].id)
+    engine.say(au_start_answer)
+    engine.runAndWait()
 
-
-def understanding_user_aurora():
-    print("How can i help boss?")
     rec = sr.Recognizer()
     with sr.Microphone() as mic:
         rec.adjust_for_ambient_noise(mic)
+        print("Pode falar")
         audio = rec.listen(mic)
-
-
     try:
         texto = rec.recognize_google(audio, language='pt-BR').lower()
-        time.sleep(1)
-        print(f"You said  {texto.upper()}")
+        return texto
     except sr.UnknownValueError:
-        print("I don't understand sorry.")
-        return ""
-
+        iris_start_error = "Não entendi, tente novamente!"
+        engine = pyttsx3.init()
+        engine.setProperty('voice', engine.getProperty('voices')[0].id)
+        engine.say(iris_start_error)
+        engine.runAndWait()
+        return ''
 
 und_user_au = ''
 if st_au:
-    und_user_au = understanding_user_aurora()
-    if und_user_au:
-        print(f"User input: {und_user_au}")
+    und_user_au = understanding_user_iris()
+    print(und_user_au)
 
 
 
+########################################################
 
 def search_aurora(searchi):
-    print("Lets search this for you my brother!")
-    time.sleep(1.5)
     url = f"https://www.google.com/search?q={searchi}"
     webbrowser.open(url)
 
 
-search_term = ''
+search_term = ""
 if und_user_au:
     for sinonimo in pesquisar_e_sinonimos:
         if sinonimo in und_user_au:
-            search_term = und_user_au.split(sinonimo, 1)[-1].strip() 
-            if search_term: 
+            search_term = und_user_au.split(sinonimo, 1)[-1].strip()
+            if search_term:  
                 search_aurora(search_term)
-            break 
+            break
+
