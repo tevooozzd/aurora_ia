a5815e4e5d3d02d7950e62337e77db93aa661775
tevoshw <estettv155@gmail.com>
2024-10-15 14:31:27 -0300

    Update and rename main_iniciando.py to second.py

---
+++ second.py
@@ -0,0 +1,8 @@
+
+################### - Recebe o valor da variavel texto (aonde está armazenado a fala do usuario) 
+################### - Identifica se é um chamado, saudação, os dois juntos e etc
+
+
+
+def entendendo_frase_sexta(texto):
+    pass
